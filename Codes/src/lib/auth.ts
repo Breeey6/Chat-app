f55c0c0a4cfe0f0e9717b05e4785c834edffa927
Breeey6 <brianluiz568@gmail.com>
2024-11-02 09:17:15 +0300

    Add files via upload

---
+++ Codes/src/lib/auth.ts
@@ -0,0 +1,78 @@
+import { NextAuthOptions } from 'next-auth'
+import { UpstashRedisAdapter } from '@next-auth/upstash-redis-adapter'
+import { db } from './db'
+import GoogleProvider from 'next-auth/providers/google'
+import { fetchRedis } from '@/helpers/redis'
+
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  try{
+    //check if credentials are available
+    if(clientId && clientSecret) {
+      return { clientId, clientSecret };
+    } else {
+      //if not, throw error with valid client id and secret
+      throw new Error('Missing Google Client ID or Secret');
+    }
+  }catch(e) {
+    console.error('Error getting Google credentials:', e);
+    throw e;
+  
+}
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUserResult = (await fetchRedis('get', `user:${token.id}`)) as
+        | string
+        | null
+
+      if (!dbUserResult) {
+        if (user) {
+          token.id = user!.id
+        }
+
+        return token
+      }
+
+      const dbUser = JSON.parse(dbUserResult) as User
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    redirect() {
+      return '/dashboard'
+    },
+  },
+}
